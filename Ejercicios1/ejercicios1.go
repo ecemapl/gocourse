538906d63c2227b5bf7a42cd7fe20404a7434ccd
ecemapl <ecemapl@gmail.com>
2023-06-02 23:28:34 +0200

    Ejercicio 2: suma de multiplos de 3 y 5 pero no de 15

--- Ejercicios1/ejercicios1.go
+++ Ejercicios1/ejercicios1.go
@@ -14,8 +14,26 @@ func devuelveSigno(param int) string {
 	}
 }
 
+// Función que, considerando la serie aritmética entre dos números
+// Calcula la suma de todos los múltiplos de 3 y de 5 pero que no sean
+// múltiplos de 15
+func sumaMultiplos3y5no15(a, b int) int {
+	suma := 0
+	for i := a; i <= b; i++ {
+		if i%15 == 0 {
+			continue
+		} else if i%3 == 0 || i%5 == 0 {
+			suma = suma + i
+		}
+	}
+	return suma
+}
+
 func main() {
 	fmt.Println(devuelveSigno(10))
 	fmt.Println(devuelveSigno(-1))
 	fmt.Println(devuelveSigno(0))
+
+	// Imprime suma de multiplos de 3 y de 5 pero no de 15 entre 0 y 300000
+	fmt.Printf("La suma de multiplos de 3 y 5, pero no de 15, entre %d y %d es %d\n", 0, 300000, sumaMultiplos3y5no15(0, 300000))
 }
