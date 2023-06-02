4ecb819f0b99d1a1ef1d49fb42a639804552cdb0
ecemapl <ecemapl@gmail.com>
2023-06-02 15:36:36 +0200

    test

--- calculaminimo.go
+++ calculaminimo.go
@@ -13,7 +13,7 @@ func getMin(a, b int) int {
 
 // Representa la tabla de multiplicar del numero indicado entre 1 y 10
 func tablaMultiplicar(n int) {
-	for i := 0; i <= 9; i++ {
+	for i := 0; i <= 10; i++ {
 		fmt.Printf("%2d x %2d = %4d\n", i, n, i*n)
 	}
 }
